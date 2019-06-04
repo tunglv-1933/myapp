f60e87e97d3bc58529013ff19484555d1fde9054
levantungb <le.van.tungb@sun-asterisk>
2019-06-04 07:16:39 +0700

    init project

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/aboutrtttt'
+  resources :microposts
+  resources :users
+  root 'application#hello'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+end
