11c6831393da9709cc773f66457a238638ff330a
levantungb <le.van.tungb@sun-asterisk>
2019-06-04 07:23:06 +0700

    complete chapter 5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_012641) do
+ActiveRecord::Schema.define(version: 2019_06_04_060456) do
 
   create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.text "content"
