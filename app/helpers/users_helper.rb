10af5367910afbb34674d1be69c227845cc5bf1c
levantungb <le.van.tungb@sun-asterisk>
2019-06-04 16:23:15 +0700

    # This is a combination of 5 commits. # The first commit's message is: finished sign up
    # This is the 2nd commit message:
    
    add setting, convention
    
    # This is the 3rd commit message:
    
    fix and add translate
    
    # This is the 4th commit message:
    
    fix
    
    # This is the 5th commit message:
    
    fix

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravatar_for user, size: Settings.size_gravatar
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "#{Settings.link.gavatar}#{gravatar_id}?s=#{size}"
+    image_tag gravatar_url, alt: user.name, class: "gravatar"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_060456) do
+ActiveRecord::Schema.define(version: 2019_06_04_070311) do
 
-  create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
