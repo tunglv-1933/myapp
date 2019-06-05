606bbfb980fe75de8b183b8435d6e037a5c0a63a
levantungb <le.van.tungb@sun-asterisk>
2019-06-05 16:17:10 +0700

    implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,15 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by email: params[:session][:email].downcase
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       flash[:success] = t "login_success"
       log_in user
       redirect_to user
     else
       flash[:danger] = t "invalid_login"
-      render "new"
+      render :new
     end
   end
 
