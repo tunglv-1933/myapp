6f01a5a9e32462ffbe098eb9e46ea1a488c23b50
levantungb <le.van.tungb@sun-asterisk>
2019-06-06 15:15:09 +0700

    implement updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i(show update destroy)
-  before_action :logged_in_user, only: %i(edit, update)
-  before_action :correct_user, only: %i(edit, update)
+  before_action :logged_in_user, only: %i(edit update)
+  before_action :correct_user, only: %i(edit update)
   before_action :admin_user, only: %i(destroy)
 
   def index
