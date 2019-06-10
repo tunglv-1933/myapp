19cc44fd534982d9d8b59b6b5a4d0ccb0fa63b83
levantungb <le.van.tungb@sun-asterisk>
2019-06-10 09:52:47 +0700

    implement account-activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: Settings.validates.name.lenght }
   VALID_EMAIL_REGEX = Settings.validates.email.regex
   validates :email, presence: true, length: { maximum: Settings.validates.email.lenght }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: Settings.validates.password.lenght.minimum }
-  validates :password_confirmation, presence: true, length: { minimum: Settings.validates.password.lenght.minimum }
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :password, presence: true, length: { minimum: Settings.validates.password.lenght.minimum }, allow_nil: true
 
   has_secure_password
 
